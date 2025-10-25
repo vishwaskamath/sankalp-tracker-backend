ec7ea03949f422a6fc8f05e7a6e6097310a39335
vishwas <kamathvishwasm@gmail.com>
2025-10-25 14:39:51 +0530

    Initial commit

---
+++ src/main/java/com/sankalp/sankalp_tracker/SankalpTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sankalp.sankalp_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SankalpTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SankalpTrackerApplication.class, args);
+	}
+
+}
